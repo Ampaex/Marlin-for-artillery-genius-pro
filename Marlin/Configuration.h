9644ebac47bdc8e542581333c43299a5d27b1c96
Antonio Vázquez Pérez <antonio.vazquez-perez@siemens.com>
2026-04-13 15:25:40 +0200

    Update PID and steps per mm in configuration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -727,9 +727,9 @@
     #define DEFAULT_KI_LIST {   1.08,   1.08 }
     #define DEFAULT_KD_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_KP  22.20
-    #define DEFAULT_KI   1.08
-    #define DEFAULT_KD 114.00
+    #define DEFAULT_KP  24.81
+    #define DEFAULT_KI   3.36
+    #define DEFAULT_KD  46.15
   #endif
 #else
   #define BANG_MAX 255    // Limit hotend current while in bang-bang mode; 255=full current
@@ -827,9 +827,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_BED_KP  47.0143
-  #define DEFAULT_BED_KI   6.3191
-  #define DEFAULT_BED_KD 233.1900
+  #define DEFAULT_BED_KP  42.4679
+  #define DEFAULT_BED_KI   5.0080
+  #define DEFAULT_BED_KD 240.0085
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #else
@@ -1366,7 +1366,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 15, 40 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 10, 40 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
